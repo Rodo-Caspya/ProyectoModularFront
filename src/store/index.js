7155927d13d9e8ba0f3df8b6a590b0735d37f507
Rodo-Caspya <rodolfoam_@outlook.com>
2022-01-10 08:44:34 -0600

    modules store
    Cambios en la store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import { createStore } from 'vuex'
 
-import loginStore from './login'
+import loginModule from '../modules/home/store/homeModule'
 import registerStore from './register'
 import mainStore from './main'
 import resetPasswordStore from './resetPassword'
 
 export default createStore({
     modules: {
-        login: loginStore,
-        register: registerStore,
-        main: mainStore,
-        reset: resetPasswordStore
+        loginModule,
+        
     }
 })
